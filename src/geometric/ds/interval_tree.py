591c8a9708732f5c06591f8adb44d5ae8fc3c900
Lucas Moretto <lucasmoretto16@gmail.com>
2019-05-23 14:54:41 -0300

    Static Interval Tree implementation is finished

--- src/geometric/ds/interval_tree.py
+++ src/geometric/ds/interval_tree.py
@@ -1,11 +1,17 @@
+from functools  import cmp_to_key
+from statistics import median
+
+from src.geometric.prim.interval import Interval
+
+
 class IntervalTree:
 
     class __Node:
 
-        def __init__(self, x_mid, intervals):
+        def __init__(self, x_mid, i_mid):
             self.x_mid     = x_mid
-            self.left_mid  = None
-            self.right_mid = None
+            self.left_mid  = sorted(i_mid, key=cmp_to_key(Interval.min_order))
+            self.right_mid = sorted(i_mid, key=cmp_to_key(Interval.max_order))
 
             # Children
             self.left  = None
@@ -17,6 +23,78 @@ class IntervalTree:
 
         intervals   = set(intervals)
         self.__size = len(intervals)
+        self.__root = self.__build(intervals)
 
     def __len__(self):
         return self.__size
+
+    def __build(self, intervals):
+        if len(intervals) == 0:
+            return None
+
+        points = set()
+        for i in intervals:
+            points.add(i.min)
+            points.add(i.max)
+
+        x_mid  = median(points)
+
+        i_mid   = self.__build_mid_intervals(x_mid, intervals)
+        i_left  = self.__build_left_intervals(x_mid, intervals)
+        i_right = self.__build_right_intervals(x_mid, intervals)
+
+        v       = self.__Node(x_mid, i_mid)
+        v.left  = self.__build(i_left)
+        v.right = self.__build(i_right)
+
+        return v
+
+    @staticmethod
+    def __build_mid_intervals(x_mid, intervals):
+        i_mid = list()
+        for i in intervals:
+            if x_mid in i:
+                i_mid.append(i)
+        return i_mid
+
+    @staticmethod
+    def __build_left_intervals(x_mid, intervals):
+        i_left = list()
+        for i in intervals:
+            if x_mid > i.max:
+                i_left.append(i)
+        return i_left
+
+    @staticmethod
+    def __build_right_intervals(x_mid, intervals):
+        i_right = list()
+        for i in intervals:
+            if x_mid < i.min:
+                i_right.append(i)
+        return i_right
+
+    def query(self, point):
+        if point is None:
+            raise ValueError("Invalid argument 'point' of None Type")
+        res = set()
+        self.__query(self.__root, point, res)
+        return res
+
+    def __query(self, v, point, res):
+        if v is None:
+            return
+
+        if point < v.x_mid:
+            for i in v.left_mid:
+                if point in i:
+                    res.add(i)
+                else:
+                    break
+            self.__query(v.left, point, res)
+        else:
+            for i in reversed(v.right_mid):
+                if point in i:
+                    res.add(i)
+                else:
+                    break
+            self.__query(v.right, point, res)
--- test/geometric/ds/test_interval_tree.py
+++ test/geometric/ds/test_interval_tree.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from src.geometric.ds.interval_tree import IntervalTree
@@ -40,3 +41,44 @@ class TestIntervalTree(unittest.TestCase):
         tree = IntervalTree(intervals)
         self.assertEqual(1000, len(tree))
 
+    def test_query_withNoneTypeArgument_shouldRaiseValueError(self):
+        intervals = list()
+        for i in range(10):
+            intervals.append(Interval(i, i))
+        tree = IntervalTree(intervals)
+
+        with self.assertRaises(ValueError):
+            tree.query(None)
+
+    def test_query_withPointNotInAnyInterval_shouldReturnEmptySet(self):
+        leftmost  = None
+        rightmost = None
+        intervals = list()
+        for i in range(1000):
+            a = Interval(random.uniform(0, i), random.uniform(i, 1000))
+            if leftmost is None or a.min < leftmost:
+                leftmost = a.min
+            if rightmost is None or a.max > rightmost:
+                rightmost = a.max
+            intervals.append(a)
+
+        tree = IntervalTree(intervals)
+        self.assertEqual(set(), tree.query(leftmost  - 1))
+        self.assertEqual(set(), tree.query(rightmost + 1))
+
+    def test_query_withPointInSomeIntervals_shouldReturnSetWithIntervalsThatContainQueryPoint(self):
+        point     = random.uniform(0, 1000)
+        expected  = set()
+        intervals = list()
+        for i in range(1000):
+            a = Interval(i, 1000)
+            b = Interval(random.uniform(0, 1000), random.uniform(0, 1000))
+            if point in a:
+                expected.add(a)
+            if point in b:
+                expected.add(b)
+            intervals.append(a)
+            intervals.append(b)
+
+        tree = IntervalTree(intervals)
+        self.assertEqual(expected, tree.query(point))
